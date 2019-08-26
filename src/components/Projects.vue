08c97d1b913537cc8dbc0fdc399acd2cf3824dd2
syntappz <doddsanater87@gmail.com>
2019-08-26 16:59:21 +0100

    changed project layout

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="projects">
-    <h1>{{ title }}</h1>
-    <div class="project-wrap">
-      <div class="project"  v-for="project in projects" :key="project.id" @mouseenter="hoverOn = true" @mouseleave="hoverOn = false">
-        <img :src="project.img" alt="woi" />
-        <div class="info">
-          <div class="text-wrap" v-if="hoverOn">
-            <h2 class="title animated fadeInDown fast">{{ project.title }}</h2>
+  <div class="projects" >
+    <h1 v-if="onProjects" class="animated flipInX">{{ title }}</h1>
+    <div class="project-wrap" v-if="onProjects">
+      <div class="project"  v-for="project in projects" :key="project.id">
+        <div class="img-wrap animated bounceInLeft" :class="project.delay">
+           <img :src="project.img" alt="woi" />
+        </div>
+       
+        <div class="info animated bounceInRight" :class="project.delay">
+          <div class="text-wrap">
+            <h2 class="title">{{ project.title }}</h2>
 
-            <div class="animated fadeInUp fast">
+            <div class="animated">
                <p>{{ project.subtext }}</p>
             <div class="more-btn">
               view more
@@ -31,35 +34,43 @@ export default {
     return {
       title: "Projects",
       hoverOn: false,
+       onProjects: false,
       projects: [
         {
           id: 1,
           title: 'world of information',
           subtext: 'Vue - Wiki/Imgur API',
-          img: 'woi-home.png'
+          img: 'woi-home.png',
+          delay: 'delay1'
         },
+      
           {
           id: 2,
-          title: 'the vault',
-          subtext: 'Vue/Vuetify - Firebase',
-          img: 'vault-notes.png'
-        },
-          {
-          id: 3,
           title: 'angels & demons',
           subtext: 'Vue - Wiki API',
-          img: 'a&d-main.png'
+          img: 'a&d-main.png',
+          delay: 'delay2'
+        },
+            {
+          id: 3,
+          title: 'the vault',
+          subtext: 'Vue/Vuetify - Firebase',
+          img: 'vault-notes.png',
+          delay: 'delay3'
         },
           {
           id: 4,
           title: 'fusion ninja',
           subtext: 'cordova/framwork7',
-          img: 'fusion.png'
+          img: 'fusion.png',
+          delay: 'delay4'
         }
       ]
     };
   },
   mounted() {
+   
+
     let pageTop = document.querySelector(".projects").offsetTop;
     this.$store.dispatch("projectsPageHeight", pageTop);
     let aboutPage = this.$store.state.aboutPageHeight;
@@ -67,6 +78,9 @@ export default {
       if (scrollY >= aboutPage) {
         pageTop = document.querySelector(".projects").offsetTop;
         this.$store.dispatch("projectsPageHeight", pageTop);
+      }
+      if(scrollY >= pageTop) {
+        this.onProjects = true;
       }
     });
   },
@@ -79,49 +93,40 @@ export default {
 <style scoped>
 .projects {
   min-height: 100vh;
-
-  padding-top: 80px;
+  padding: 80px 0;
   box-sizing: border-box;
   background: linear-gradient(0deg, #015169, #003849);
-
   color: #333;
 }
 .project-wrap {
-  width: 80%;
-  margin: auto;
- 
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  align-content: space-around;
+  width: 70%;
+  margin: 100px auto;
 }
 h2 {
+  text-align:center;
   text-transform: uppercase;
 }
 .project {
-  width: 47%;
-  height: 300px;
-  margin:15px 0;
-  border: solid 1px #fff;
-  position: relative;
+  width: 100%;
+  display:flex;
+  margin:20px auto;
+  height:300px;
+  
+ transition: .3s ease;
 }
+
 
 .info {
-  display: flex;
+  width:50%;
+  background:rgb(255, 255, 255);
+  display:flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  height: 100%;
-  background: rgba(255, 255, 255, 0.836);
-  position: absolute;
-  opacity: 0;
-  transition: 1s ease;
-  
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+ 
 }
 
-.project:hover .info {
-  opacity: 1;
-}
 
 h1 {
   font-weight: 600;
@@ -129,15 +134,18 @@ h1 {
   color: #fff;
   padding: 0;
 }
+.img-wrap {
+  width:50%;
+  
+}
 img {
   object-fit: cover;
   width: 100%;
   height: 300px;
-  cursor: pointer;
-  transition: 1s;
-  position: absolute;
-  left: 0;
-  top: 0;
+   border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+  transition: .3s ease;
+ 
 }
 
 
@@ -151,6 +159,25 @@ img {
   color: #fff;
   border: solid 2px #00b7a1;
   background: #00b7a1;
+}
+
+.delay1 {
+  animation-delay: .4s;
+}
+.delay2 {
+  animation-delay: .6s;
+}
+.delay3 {
+  animation-delay: .8s;
+}
+.delay4 {
+  animation-delay: 1s;
+}
+@media (min-height: 800px) {
+  .project-wrap {
+    width: 60%;
+  }
+  
 }
 @media (max-width: 1024px) {
   .project-wrap {
